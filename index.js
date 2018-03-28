485c929cfb1d90357c0012f7947d57b5b68ae7e8
Cezar Vinícius de O. Pretto <cezar@planosassessoria.com.br>
2018-03-28 08:48:26 -0300

    Revert "new methods and params to update products in Planos Contagem Database"

--- index.js
+++ index.js
@@ -5,28 +5,22 @@ const services = require('./services')
 
 commander
   .version(version)
-  .option('-h, --host <host>', 'Host address')
-  .option('-H, --HOST <HOST>', 'Planos Host address')
+  .option('-H, --host <host>', 'Host address')
   .option('-p, --port <port>', 'Database port')
-  .option('-P, --PORT <PORT>', 'Planos Database port')
   .option('-d, --database <database>', 'Database name')
-  .option('-D, --DATABASE <DATABASE>', 'Planos Database name')
   .option('-u, --user <user>', 'Database username')
-  .option('-U, --USER <USER>', 'Planos Database username')
-  .option('-w, --password <password>', 'Database password')
-  .option('-W, --PASSWORD <PASSWORD>', 'Planos Database password')
+  .option('-P, --password <password>', 'Database password')
   .option('-o, --output <outputPath>', 'Output path')
-  .option('-E, --EMPRESA <EMPRESA>', 'CNPJ Empresa Database Planos')
   .option('-s, --system <systemName>', 'The system name. Example: fire, ciss')
   .parse(process.argv)
 
-const start = async ( { host, HOST, port, PORT, database, DATABASE, user, USER, password, PASSWORD, EMPRESA } ) => {
+const start = async ({host, port, database, user, password}) => {
   try {
-    const params = { host, HOST, port, PORT, database, DATABASE, user, USER, password, PASSWORD, EMPRESA }
-    const response = await services[ commander.system ].getReport( params, commander.output )
-    console.log( response )
-  } catch ( e ) {
-    console.error( e )
+    const params = { host, port, database, user, password }
+    const response = await services[commander.system].getReport(params, commander.output)
+    console.log(response)
+  } catch (e) {
+    console.error(e)
   }
 }
 
